5beb670880d9e197f30f35fd9376d478d375dee1
alejandro314 <manuelmartines385@gmail.com>
2025-03-30 17:58:00 -0500

    PRACTICA 1 (VENTAS PARA RESTAURANTE)

--- ejercicio/practicas.py
+++ ejercicio/practicas.py
@@ -1,10 +1,81 @@
-burger = 2.o
-hot dog =
-pizza = 2.99
-coke = 0.99
 
-total = pizza + coke
+fecha = "30 de Marzo del 2025"
+print(fecha)
 
-tip = total * 0.2
+presentacio = "Bienvenidos a Master burger"
+print(presentacio)
+print("        MENU             ")
 
-print(tip) # Output: 0.796
+papitas = 10.000
+burger = 20.000
+hot_dog = 18.000
+pizza = 30.000
+coke = 5.000
+bebidas = 5.000
+
+print("\n1 papitas = 10.000\n2"
+" burger = 20.000 \n3 hot_dog = 18.000 \n4 pizza = 30.000 \n5 coke = 5.000 \n6 bebidas = 5.000")
+ 
+
+pedido = input("ingrese su pedido  ")
+
+if pedido in ["1","2","3","4","5","6"]:
+    print("Preparando Pedido...")
+
+    print("su pedido esta listo...")
+else :
+    print("Pedido incorrecto")
+
+    #cobro de cuenta
+
+print("total a pagar")
+
+papitas = 10000
+burger = 20000
+hot_dog = 18000
+pizza = 30000
+coke = 5000
+bebidas = 5000
+
+if pedido in["1"]:
+    print (papitas)
+
+elif pedido in["2"]:
+   print(burger)
+elif pedido in["3"]:
+    print(hot_dog)
+elif pedido in["4"]:
+   print(pizza)
+elif pedido in["5"]:
+    print(coke)
+elif pedido in["6"]:
+    print (bebidas)
+else:
+    print("Pedido incorrecto")
+
+fin = "valor con propina voluntaria"
+print(fin)
+propina_1 = 11000
+propina_2 = 22000
+propina_3 = 19800
+propina_4 = 33000
+propina_5 = 5500
+propina_6 = 5500
+
+if pedido in["1"]:
+    print (propina_1)
+
+elif pedido in["2"]:
+   print(propina_2)
+elif pedido in["3"]:
+    print(propina_3)
+elif pedido in["4"]:
+   print(propina_4)
+elif pedido in["5"]:
+    print(propina_5)
+elif pedido in["6"]:
+    print (propina_6)
+else:
+    print("Pedido incorrecto")
+
+print ( "GRACIAS PRO SU COMPRA :)")
